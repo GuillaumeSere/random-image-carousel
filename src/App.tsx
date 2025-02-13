628a2422404147afde2cb8a1f8711a25eae5b5eb
Guillaume Sere <guillaumesere@hotmail.fr>
2025-02-13 13:49:51 +0100

    add image nasa

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import './App.css';
 import ImageCarousel from './components/ImageCarousel';
+import ImageCarouselFade from './components/ImageCarouselFade';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Random Image Carousel</h1>
       <ImageCarousel />
+      <ImageCarouselFade />
     </div>
   );
 };
---
+++ src/components/ImageCarouselFade.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const ImageCarouselFade: React.FC = () => {
+  const [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const imageUrls: string[] = [];
+      for (let i = 0; i < 10; i++) {
+        const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=1B13o5S3g0GI5asvqdYiznyahC3GOnzrcxm760zy`);
+        const data = await response.json();
+        imageUrls.push(data.url);
+      }
+      setImages(imageUrls);
+    };
+
+    fetchImages();
+  }, []);
+
+
+  return (
+    < >
+        <div className="box-image">
+          <img src={images[0]} style={{ width: '300px', height: 'auto' }} />
+          <img src={images[0]} style={{ width: '300px', height: 'auto' }} />
+        </div>
+    </>
+  );
+};
+
+export default ImageCarouselFade;
+
